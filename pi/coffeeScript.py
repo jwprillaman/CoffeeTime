8ddc227c14730eb9dee33fb13bdad45572e5b937
jwprillaman <jwprillaman@gmail.com>
2015-02-01 03:02:00 -0600

    added sensor pickup

--- pi/coffeeScript.py
+++ pi/coffeeScript.py
@@ -3,7 +3,12 @@ import fcntl
 import struct
 import sys
 import RPi.GPIO as GPIO
-PIN = 11
+
+REY_PIN = 11
+SEN_PIN = 15
+
+
+
 if len(sys.argv) < 2 :
   sys.exit('Usage: %s interface' % sys.argv[0])
 
@@ -22,18 +27,34 @@ def get_ip_address(ifname):
       )[20:24])
 
 #set GPIO properties
-def setGPIO(PIN):
+def setGPIO(SEN_PIN, REY_PIN):
       GPIO.setmode(GPIO.BOARD)
-      GPIO.setup(PIN, GPIO.OUT)
-      print 'GPIO set:  mode = Board; %s = GPIO.OUT' %PIN
+      GPIO.setup(REY_PIN, GPIO.OUT)
+      GPIO.setup(SEN_PIN, GPIO.IN)
 
+#listen for client alarm 1
 def receiveAlarm(client):
       data = client.recv(1024)
       print data
       if 'coffeeTime' in data:
-        GPIO.output(PIN,1)
-        
-setGPIO(PIN)
+        GPIO.output(REY_PIN,1)
+
+# turn on the coffee sensor
+def turnOnSensor(client):
+  count = 0
+  while 1:
+    if GPIO.input(SEN_PIN):
+      print "motion detected"
+      count = count + 1
+    if count > 4:
+      break
+  client.send("PickedUp");
+
+
+#START Setup
+
+setGPIO(REY_PIN)
+
 print get_ip_address(IF)
 
 #create socket
@@ -61,6 +82,7 @@ while True:
   print 'Got connection from', addr
   
   client.send('Begin Brewing')
+  
   receiveAlarm(client)
-
+  turnOnSensor(client)
   client.close()
