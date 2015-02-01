5f21196674636a6d107e94f44d11941b8588403b
jwprillaman <jwprillaman@gmail.com>
2015-02-01 03:59:28 -0600

    sonar added testing

---
+++ pi/sonar.py
@@ -0,0 +1,33 @@
+import RPI.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+TRIG = 16
+ECHO = 18
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+
+print "Sensor is settling"
+time.sleep(2)
+
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+
+distance = round(distance, 2)
+
+print "Distance: ", distance, "cm"
+
+GPIO.cleanup()
